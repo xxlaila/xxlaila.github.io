a79759ec00d838ab387c1a5844eba39b58582332
xxlaila <cq_xxlaila@163.com>
2020-02-20 16:41:52 +0800

    修改和增加Elasticsearch

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Feb 17 2020 19:10:36 GMT+0800 (China Standard Time)
+// build time:Thu Feb 20 2020 16:22:33 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
