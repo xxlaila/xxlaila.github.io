8fd43c647707d3d7572db9321126d3270b9497cf
xxlaila <cq_xxlaila@163.com>
2020-05-09 13:43:32 +0800

    增加mongodb 副本集数据恢复

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu May 07 2020 18:10:04 GMT+0800 (CST)
+// build time:Sat May 09 2020 13:40:36 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
